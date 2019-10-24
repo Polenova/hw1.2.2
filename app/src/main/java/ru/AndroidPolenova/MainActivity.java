0fa0c7a5375e531a82d192bb03731b2b6eb0218a
Marina <M.V.Polenova@users.noreply.github.com>
2019-10-24 22:52:03 +0200

    Initial commit

---
+++ app/src/main/java/ru/AndroidPolenova/MainActivity.java
@@ -0,0 +1,42 @@
+package ru.AndroidPolenova;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView textView = (TextView) findViewById(R.id.textNext);
+        int rnd = ThreadLocalRandom.current().nextInt(0, 100);
+        textView.setText("http://myfile.org/" + rnd);
+        Button btnNext = (Button) findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btnBack = (Button) findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_MAIN);
+                i.addCategory(Intent.CATEGORY_HOME);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
+    }
+}
