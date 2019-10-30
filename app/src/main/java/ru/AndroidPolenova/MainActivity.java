8288e7d13c15a77b3adc50a78cfd77377ba33928
Marina <M.V.Polenova@users.noreply.github.com>
2019-10-30 10:59:08 +0100

    homework1.2.2 edit

--- app/src/main/java/ru/AndroidPolenova/MainActivity.java
+++ app/src/main/java/ru/AndroidPolenova/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
